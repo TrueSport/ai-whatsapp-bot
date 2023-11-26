dc9ea1d8fdc6aab012ae2b200f6f23aa67bb6815
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-11-26 13:11:58 +0000

    more travis fixes

---
+++ src/message/customer/customer.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class CustomerService {
+  constructor(private readonly prisma: PrismaService) {}
+  async findCustomer(id: string) {
+    return await this.prisma.customer.findFirst({
+      where: {
+        user: id,
+      },
+      include: {
+        messages: {
+          select: {
+            role: true,
+            content: true,
+          },
+        },
+      },
+    
