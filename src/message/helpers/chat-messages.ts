66ae30efc636c071c69f18c04df0c3fe7bce7bf1
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-12-01 09:06:39 +0000

    Bump version to 1.6.1

--- src/message/helpers/chat-messages.ts
+++ src/message/helpers/chat-messages.ts
@@ -19,4 +19,32 @@ function getDonationMessage(): string {
   return `
     🙌 Olá! Você ama a ideia de IA WhatsApp que pode ajudar no dia a dia? 🤖
   
-    🎉 Nós estamos construindo isso agora! Mas para continuar p
+    🎉 Nós estamos construindo isso agora! Mas para continuar precisamos de sua ajuda.
+      
+    👉 Cada doação é importante e ajuda a manter e aprimorar o projeto. Use a chave PIX abaixo para fazer uma doação agora mesmo e faça parte da nossa missão de tornar IA's acessíveis para todos.
+  
+    🚀 Sua contribuição fará uma grande diferença para nós e para a comunidade. Obrigado pela sua generosidade! 😊
+  
+    🙏 Basta enviar a mensagem doar ou /doar🙏 `;
+}
+
+function getThankYouMessage(): string {
+  return `
+    🥳🥳 Muitooo obrigado 🥳🥳
+      
+    PIX Numero: (51)9 9780-5917
+  
+    Sua contribuição é essencial
+    para mantermos o projeto!
+        `;
+}
+
+function getClearChatSuccessMessage(): string {
+  return `Histórico limpo com sucesso, como posso te ajudar hoje? `;
+}
+
+function getHelperSuporterMessage(): string {
+  return `Para suporte humanizado, entre em contato através dos seguintes números durante o horário comercial:
+
+  📞 +55 64 9 8123-0208 - Falar com Gustavo
+  📞 +55 51 9 9780-5917
