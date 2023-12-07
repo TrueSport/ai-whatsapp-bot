da5173a672e87a1f2072b41f87a1f6daf5eb14fb
TrueSport <118295161+TrueSport@users.noreply.github.com>
2023-12-07 20:01:37 +0000

    Add docstring.

--- src/message/message.service.ts
+++ src/message/message.service.ts
@@ -63,4 +63,51 @@ export class MessageService {
       },
     });
 
-    const context = await this.customerService.getMessagesContex
+    const context = await this.customerService.getMessagesContext(user);
+    const response = await this.openAiService.createChatCompletition(
+      user,
+      context,
+      content,
+    );
+    if (response === 400) {
+      this.customerService.clearHistory(user);
+      return getServiceUnavailableError();
+    }
+    if (!response || response === 429) {
+      return getServiceUnavailableError;
+    }
+
+    await this.customerService.saveMessage({
+      role: 'assistant',
+      content: response,
+      owner: {
+        connect: {
+          user: user,
+        },
+      },
+    });
+    return response;
+  }
+
+  async createImage(prompt: string) {
+    const createdImage = await this.openAiService.createImage(
+      prompt.substring(9),
+    );
+    return createdImage;
+  }
+
+  async helpMessage() {
+    return getHelpMessage();
+  }
+
+  async clearMessageHistory(user: string) {
+    await this.customerService.clearHistory(user);
+    return getClearChatSuccessMessage();
+  }
+
+  async donationMessage() {
+    return getThankYouMessage();
+  }
+
+  async HelperSuporterMessage() {
+    r
